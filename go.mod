dc6b552ee8ff49f6bf882ff63e1d8d7c93f19b55
Sergei Stadnik <sergeisqa@gmail.com>
2020-05-05 17:27:34 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/monkrus/gin-2
+
+go 1.14
---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/monkrus/gin/controller"
+	"github.com/monkrus/gin/service"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+}
